3e211fcdda177fcd8c2abf0b90ab45c13f2b29d7
Pierre Reynaud <pierre.reynaud@outlook.com>
2020-01-25 19:11:50 +0100

    Add custom config

--- src/common/config.js
+++ src/common/config.js
@@ -1,2 +1,3 @@
-export const API_URL = "https://conduit.productionready.io/api";
+// export const API_URL = "https://conduit.productionready.io/api";
+export const API_URL = "http://vps727839.ovh.net:3000/api";
 export default API_URL;
