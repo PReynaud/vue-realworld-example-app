d846f0895de83dd234f0685e32eeb89c866e5b1c
Pierre Reynaud <pierre.reynaud@outlook.com>
2020-01-26 16:21:27 +0100

    Use https URL

--- src/common/config.js
+++ src/common/config.js
@@ -1,3 +1,4 @@
 // export const API_URL = "https://conduit.productionready.io/api";
-export const API_URL = "http://vps727839.ovh.net:3000/api";
+// export const API_URL = "http://vps727839.ovh.net:3000/api";
+export const API_URL = "https://vps727839.ovh.net:8443/api";
 export default API_URL;
